1c7cfecb56bd060df551e8b9710ba5fb6624b120
Mbao <131330259+Mbao03@users.noreply.github.com>
2026-08-28 01:20:58 +0800

    Delete src directory

--- src/main/java/com/example/kitchen/service/RecommendService.java
+++
@@ -1,9 +0,0 @@
-package com.example.kitchen.service;
-
-import com.example.kitchen.dto.RecommendDishVO;
-
-import java.util.List;
-
-public interface RecommendService {
-    List<RecommendDishVO> recommendByUserId(Integer userId, Integer limit);
-}
